3ea712558c831fb14caa3fd8f33914a2bc494f05
muhammadzohaibr556 <muhammadzohaibr556@gmail.com>
2020-06-28 15:15:54 -0700

    django-react-implementation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import BaseRouter from "./routes";
+
+function App() {
+  return (
+    <Router>
+      <BaseRouter />
+    </Router>
+  );
+}
+
+export default App;
